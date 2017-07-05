8ead26c86c9b39be423ae26182a7ac7f24d10eb4
xawen <jmck@xawen.net>
2017-07-05 15:59:17 -0400

    Add "Next Certificate Expiration" channel

--- NetScaler-CertExpiration.ps1
+++ NetScaler-CertExpiration.ps1
@@ -11,9 +11,24 @@ $Session =  Connect-Netscaler -Hostname $Nsip -Credential $Credential -PassThru
 
 $CertResults = Get-NSSSLCertificate -session $Session | Where-Object {$_.certificatetype -eq "CLIENTANDSERVER_CERT"} 
 
-$today = Get-Date
+$FirstExpiration = 2000
+foreach ($Result in $CertResults) {
+	If ($Result.daystoexpiration -lt $FirstExpiration) {$FirstExpiration=$Result.daystoexpiration}
+}
 
 Write-Host "<prtg>"
+
+Write-Host "<result>"
+Write-Host "<channel>Next Cert Expiration</channel>"
+Write-Host ("<value>" + $FirstExpiration + "</value>")
+Write-Host "<unit>Custom</unit>"
+Write-Host "<CustomUnit>Days</CustomUnit>"
+Write-Host "<LimitMode>1</LimitMode>"
+Write-Host "<LimitMinWarning>30</LimitMinWarning>"
+Write-Host "<LimitWarningMsg>Certificate expiration in less than 30 days</LimitWarningMsg>"
+Write-Host "<LimitMinError>10</LimitMinError>"
+Write-Host "<LimitErrorMsg>Certificate expiration in less than 10 days</LimitErrorMsg>"
+Write-Host "</result>"
 
 foreach ($Result in $CertResults) {
 	Write-Host "<result>"
